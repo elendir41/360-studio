cdb078781adcd7bfe398550fafbae21f6e1a53e9
clementdrs <clementdrs.dev@gmail.com>
2024-10-23 23:40:12 +0200

    clean: remove useless import and code

--- src/components/video-player/2Dcanvas.tsx
+++ src/components/video-player/2Dcanvas.tsx
@@ -3,19 +3,12 @@ import React, { useEffect, useRef } from 'react'
 import VideoMesh from './video-mesh'
 import InfoTexture from './info-texture'
 import useVideoPlayerStore from '~/store/video-player-store'
-import useTimelineStore from '~/store/timeline-store'
-import useGetCurrentVideoMedia from './hooks/useGetCurrentVideoMedia'
-import { Slider } from 'react-aria-components'
 
 const TwoDCanvas = () => {
   const canvasRef = useVideoPlayerStore((state) => state.canvasRef);
   const setCanvasRef = useVideoPlayerStore((state) => state.setCanvasRef);
   const initialiseMediaRecorder = useVideoPlayerStore((state) => state.initialiseMediaRecorder);
   const ref = useRef(null);
-  const currentMedia = useGetCurrentVideoMedia();
-
-  const videoTexture = useVideoPlayerStore((state) => state.videoTexture);
-  const [currentTime, setCurrentTime] = React.useState(0);
 
   useEffect(() => {
     if (!canvasRef || !canvasRef.current) return;
@@ -32,9 +25,7 @@ const TwoDCanvas = () => {
     <div className='h-full'>
       <Canvas ref={ref} orthographic>
         <ambientLight intensity={1} />
-        {/* {videoTracks.items.map((media) => ( */}
           <VideoMesh/>
-        {/* ))} */}
         <InfoTexture />
       </Canvas>
     </div>
